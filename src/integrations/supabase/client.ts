f0aa44cf0c2727961a97822a12836cad0d4e3ff0
palomagl <palomalorenzon1993@gmail.com>
2026-08-06 18:13:47 -0300

    atualiza credenciais corretas do supabase

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = "https://psddwgcehiedqvtxrilz.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBzZGR3Z2NlaGllZHF2dHhyaWx6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ5MDYxNDYsImV4cCI6MjA5MDQ4MjE0Nn0.RST5ltmUtHoGNcOvo5jxNtgRa6KQyjrMpLfMlyGz9WM";
+const SUPABASE_URL = "https://ejlsbejjifbcyastjoil.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_jEq0Qhvol34PK2pXYGsbMQ_-oQ_I-yv";
 
 export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, {
   auth: {
